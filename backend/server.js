1cea1a2628cd65a8a53d946ff7e7d335adfefaa5
ankita007-coder <ankitabudhia42@gmail.com>
2023-09-08 19:47:23 +0530

    debugging

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,22 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/database.js";
 import cloudinary from "cloudinary";
+import cors from "cors"; // Import the CORS middleware
 
 const PORT = process.env.PORT || 5000;
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
 connectDatabase();
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
+// Use the CORS middleware before defining other routes
+app.use(cors());
 
-app.listen(process.env.PORT, ()=>{
-    console.log("listening on port " + process.env.PORT);
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
 });
