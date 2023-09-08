48306ad6df0dc28aa1918a4ad499437e04fea5b4
ankita007-coder <ankitabudhia42@gmail.com>
2023-09-08 18:59:43 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,24 @@
 import express from "express"; 
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 export const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.json({limit:"50mb"}));
-app.use(express.urlencoded({ extended:true, limit: "50mb"}))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
-
 import { userRouter } from "./routes/User.js";
-app.use("/api/v1",userRouter);
+app.use("/api/v1", userRouter);
 
-app.use(express.static("./frontend/build"));
+// Go up one level to the root directory and then descend into 'frontend' directory
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve("__dirname","./frontend/build","index.html"));
-})
+// Go up one level to the root directory and then descend into 'frontend' directory
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
---
+++ frontend/build/precache-manifest.e0d865b7be9082fa9b9eb2dd279dc37f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "46243b52b9409c47b1dd",
+    "url": "/static/css/main.b45efb2c.chunk.css"
+  },
+  {
+    "revision": "46243b52b9409c47b1dd",
+    "url": "/static/js/main.2d309af4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4432891a71844cf9b26",
+    "url": "/static/css/2.23cf6601.chunk.css"
+  },
+  {
+    "revision": "e4432891a71844cf9b26",
+    "url": "/static/js/2.8f84039e.chunk.js"
+  },
+  {
+    "revision": "35622a1c659daae04d93a7528621c9bb",
+    "url": "/static/media/moon.35622a1c.jpg"
+  },
+  {
+    "revision": "dec4665d989b4f28cf68afcc2d3d5c3f",
+    "url": "/static/media/venus.dec4665d.jpg"
+  },
+  {
+    "revision": "e3ff537c8c57b2ad2b8353d42875767e",
+    "url": "/static/media/space.e3ff537c.jpg"
+  },
+  {
+    "revision": "3196dfabeb44d5ea7d2aa24e277c405e",
+    "url": "/static/media/logop.3196dfab.png"
+  },
+  {
+    "revision": "00b06a057eeb9f0c2292f04b34ae085f",
+    "url": "/index.html"
+  }
+];
